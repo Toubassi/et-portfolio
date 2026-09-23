a9fc6d64d0a867f5ab6404fd20fd211ac8940c9b
Toubassi <emil_toubassi@hotmail.com>
2026-09-23 11:53:37 +0300

    portfolio

--- files/script.js
+++ files/script.js
@@ -4,8 +4,3 @@ document.addEventListener("mousemove", function (event) {
   cursorGlow.style.left = event.clientX + "px";
   cursorGlow.style.top = event.clientY + "px";
 });
-
-// const themeButton = document.querySelector("#theme-toggle");
-// themeButton.addEventListener("click", function () {
-//   document.body.classList.toggle("light-theme");
-// });
